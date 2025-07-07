e6c310f6f8a740fcc3f683a5ec8503ea274aa31e
Shreyash Gore <shreyashgore193@gmail.com>
2025-07-07 17:52:29 +0530

    Delete and update APi

--- src/app.js
+++ src/app.js
@@ -20,6 +20,59 @@ app.post("/signup",async (req,res)=>{
     };
 });
 
+//single user by mail id
+app.get("/user",async(req,res)=>{
+
+    const email=req.body.emailId;
+
+    try{
+        const user= await User.find({emailId:email});
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Something went Wrong");
+    }
+});
+
+//get feed of app
+app.get("/feed",async(req,res)=>{
+    try{
+        const users= await User.find({});
+        res.send(users);
+    }
+     catch(err){
+        res.status(400).send("Something went Wrong");
+    }
+
+})
+
+//delete an user
+app.delete("/deleteUser",async (req,res)=>{
+    const userId=req.body.userId;
+    try{
+        const user= await User.findByIdAndDelete({_id:userId});
+        res.send("user deleted Successfully");
+    }
+    catch(err){
+        res.status(400).send("something went Wrong");
+    }
+});
+
+
+//update an user data
+app.patch("/updateUser",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        const user= await User.findByIdAndUpdate({_id:userId},data);
+        res.send("user updated successfully");
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+
+    }
+})
+
 connectDB().then(()=>{
     console.log("Database connection established..");
     app.listen(3000,()=>{
